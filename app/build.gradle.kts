7782cc10d9f2122500c696b7dda3f416f2237806
Meet Patel <meetpatel4384@gmail.com>
2026-08-06 11:58:33 +0530

    Bump version to 26.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.meetpatel.popgrow"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "26.1.0"
     }
 
     androidResources {
